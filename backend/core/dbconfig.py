2069a56716aa50d41d3215e0a44edc8a6bccf31b
justtehox <zafarihox@gmail.com>
2025-11-08 15:00:05 +0330

    final v2

--- backend/core/dbconfig.py
+++ backend/core/dbconfig.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 
 def getDBconfig():
     conn_str = (
-        "postgresql+psycopg2://postgres:1234@localhost:5432/pcdb"
+        "postgresql+psycopg2://postgres:1234@localhost:5432/pcdbtwo"
         # "postgresql+psycopg2://postgres@localhost:5432/pcdb"
     )
     return create_engine(conn_str)
